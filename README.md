abdde2c19b9b491f0199b346add510fa1cf64b20
Dr Vaughan Watson <vaughanwatson@users.noreply.github.com>
2017-01-05 19:30:17 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# McKenzies
-McKenzies Solicitors
+#Typesetter CMS#
+
+Open source CMS written in PHP focused on ease of use with true WYSIWYG editing and flat-file storage.
+* [Typesetter Home](http://www.typesettercms.com)
+* [Typesetter Download](http://www.typesettercms.com/Download)
+* [Typesetter Demo](http://www.typesettercms.com/Demo)
+* [Typesetter Forum](http://www.typesettercms.com/Special_Forum)
+
+##Requirements##
+* PHP 5.3+
+* PHP Safe Mode Off
+
+##Installation##
+1. Download the latest stable release of Typesetter from TypesetterCMS.com
+
+2. Upload the extracted contents to your server
+
+3. Using your web browser, navigate to the folder you just uploaded the unzipped contents to
+
+4. Complete the installation form and submit
+
+You can find more detailed installation information on [TypesetterCMS.com](http://www.typesettercms.com/Docs/Installation)
+
+
+##Contribute##
+Submitting bug fixes and enhancements is easy:
+
+1. Log in to GitHub
+
+2. Fork the Typesetter Repository
+  * https://github.com/Typesetter/Typesetter
+  * Click "Fork" and you'll have your very own copy of the Typesetter source code at http://github.com/{your-username}/Typesetter
+
+3. Edit files within your fork.
+  This can be done directly on GitHub.com at http://github.com/{your-username}/Typesetter
+
+4. Submit a Pull Request (tell Typesetter about your changes)
+  * Click "Pull Prequest"
+  * Enter a Message that will go with your commit to be reviewed by core committers
+  * Click “Send Pull Request”
+
+###Multiple Pull Requests and Edits###
+When submitting pull requests, it is extremely helpful to isolate the changes you want included from other unrelated changes you may have made to your fork of Typesetter. The easiest way to accomplish this is to use a different branch for each pull request. There are a number of ways to create branches within your fork, but GitHub makes the process very easy:
+
+1. Start by finding the file you want to edit in Typesetter's code repository at https://github.com/Typesetter/Typesetter.
+2. Once you have located the file, navigate to the code view and click "Edit". For example, if you want to change the /include/common.php file, the "Edit" button would appear on this page: https://github.com/Typesetter/Typesetter/blob/master/include/common.php
+3. Now, edit the file as you like then click "Propose File Change"
---
+++ gpconfig.php
@@ -0,0 +1,149 @@
+<?php
+
+
+/**
+ * $upload_extensions_allow and $upload_extensions_deny
+ * Allow or deny the upload of files based on their file extensions
+ * The default list of available extenstions is array('7z', 'aiff', 'asf', 'avi', 'bmp', 'bz', 'csv', 'doc', 'fla', 'flv', 'gif', 'gz', 'gzip', 'jpeg', 'jpg', 'mid', 'mov', 'mp3', 'mp4', 'mpc', 'mpeg', 'mpg', 'ods', 'odt', 'pdf', 'png', 'ppt', 'pxd', 'qt', 'ram', 'rar', 'rm', 'rmi', 'rmvb', 'rtf', 'sdc', 'sitd', 'swf', 'sxc', 'sxw', 'tar', 'tgz', 'tif', 'tiff', 'txt', 'vsd', 'wav', 'wma', 'wmv', 'xls', 'xml', 'zip');
+ * Note: gp_restrict_uploads has to be set to true for upload_extension settings to have any effect
+ */
+define('gp_restrict_uploads',true);
+$upload_extensions_allow = array();
+$upload_extensions_deny = array();
+
+
+/**
+ * gp_default_theme
+ * Theme/color to be used when gpEasy is first installed.
+ * Also the theme/color that gpEasy will use should the user specified theme become unavailable
+ *
+ */
+define('gp_default_theme','Three_point_5/Shore');
+
+
+/**
+ * gp_browser_auth
+ * Set to true to enable additional security by requiring a static browser identity for user session. Disabled by default since gpEasy 2.3.2
+ * Enabling this feature may require administrators to log back in. If administrators report they are being logged out, then you may need to disable this feature
+ * Defaults to false
+ */
+define('gp_browser_auth',false);
+
+
+/**
+ * gp_require_encrypt
+ * Set to true to require admin area encrypted login.
+ * This will only disable un-encrypted login and not hide the option on the login page. When true, attempts to login without encryption will fail
+ * Defaults to false
+ */
+define('gp_require_encrypt',false);
+
+
+/**
+ * gp_index_filenames
+ * Set to false to use filenames that reflect the titles of pages instead using the data index
+ * Setting to false may result in the loss of functionality including the inability to use hierarchical file names
+ * Defaults to true
+ */
+define('gp_index_filenames',true);
+
+
+/**
+ * gp_remote_plugins, gp_remote_themes, gp_remote_update
+ * Disable installation of remote addons
+ * Defaults to true
+ */
+define('gp_remote_plugins',true);
+define('gp_remote_themes',true);
+define('gp_remote_update',true);
+
+
+/**
+ * gp_unique_addons
+ * Displays only the latest version of an addon (plugin or theme) if true
+ * Defaults to false
+ */
+define('gp_unique_addons',false);
+
+
+/**
+ * Using setlocale() may enable more language specific ouptput for dates, times etc
+ * http://php.net/manual/en/function.setlocale.php
+ * Defaults to en_US
+ */
+//setlocale(LC_ALL, 'en_US');
+
+
+/**
+ * service_provider_id
+ * For gpEasy.com/Special_Services
+ * Add your service provider id for tracking and to increase service provider activity level
+ * Defaults to false
+ */
+define('service_provider_id',false);
+
+
+/**
+ * Limit the number of revisions to store in the backup
+ *
+ */
+define('gp_backup_limit',30);
+
+
+/**
+ * gp_chmod_file
+ * The mode used by chmod() for data files
+ * http://php.net/manual/en/function.chmod.php
+ * Defaults to 0666
+ */
+define('gp_chmod_file',0666);
+
+/**
+ * gp_chmod_dir
+ * The mode use by chmod for data folders
+ * http://php.net/manual/en/function.chmod.php
+ * Defaults to 0755
+ */
+define('gp_chmod_dir',0755);
+
+
+/**
+ * gpdebug
+ * Set to true to display php errors in the browser window.
+ * Defaults to false
+ */
+define('gpdebug',false);
+
+
+/**
+ * gpdebugjs
+ * Set to true to display javascript errors in the browser window. Separate from gpdebug to allow specific debugging
+ * Defaults to the value set for gpdebug
+ */
+//define('gpdebugjs',false);
+
+
+
+/**
+ * Prevent errors from being displayed to site visitors
+ * Should be set to "0" for any production site
+ * Set to "1" if gpEasy is unable to display errors with gpdebug set to "true" (see above)
+ *
+ */
+@ini_set('display_errors',0);
+
+/**
+ * gp_safe_mode
+ * Set to true to turn safe mode on. This will prevent addons from being used.
+ * Defaults to the value set for gpdebug
+ */
+//define('gp_safe_mode',false);
+
+
+
+/**
+ * Include clearfloats in gpeasy generated code
+ * define('clear_floats',false); experimental
+ */
+
+
